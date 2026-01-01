b272ed3dc38cf9ba43ce734d836b88a91b97bcc1
Daniel Simmons <smileybob72801@gmail.com>
2025-12-31 22:34:23 -0800

    Add player movement
    Respects tile definition rules for walkability

--- src/Game/game.gd
+++ src/Game/game.gd
@@ -6,7 +6,7 @@ const player_definition: EntityDefinition = preload("res://assets/definitions/en
 @onready var player: Entity
 @onready var event_handler: EventHandler = $EventHandler
 @onready var entities: Node2D = $Entities
-
+@onready var map: Map = $Map
 
 func _ready() -> void:
 	var player_start_pos: Vector2i = Grid.world_to_grid(get_viewport_rect().size.floor() / 2)
@@ -15,3 +15,11 @@ func _ready() -> void:
 	var npc := Entity.new(player_start_pos + Vector2i.RIGHT, player_definition)
 	npc.modulate = Color.ORANGE_RED
 	entities.add_child(npc)
+	
+func get_map_data() -> MapData:
+	return map.map_data
+
+func _physics_process(_delta: float) -> void:
+	var action: Action = event_handler.get_action()
+	if action:
+		action.perform(self, player)
